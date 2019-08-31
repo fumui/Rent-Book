9f2c6c291566d8b4ca4407ef51d5f6f12949e6bb
Fuad Mustamirrul Ishlah 83 <amir24405@gmail.com>
2019-08-31 07:38:44 +0700

    config db

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,10 +2,10 @@ require('dotenv').config()
 
 const mysql = require('mysql')
 const conn = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: process.env.DB_HOST || 'remotemysql.com:3306',
+  user: process.env.DB_USER || 'XEEMQEoL6j',
+  password: process.env.DB_PASSWORD || 'AZm2M6aluF',
+  database: process.env.DB_NAME || 'XEEMQEoL6j'
 })
 const information_schema_conn = mysql.createConnection({
   host: process.env.DB_HOST,

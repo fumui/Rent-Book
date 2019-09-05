ac0cdd0f85c4fa2a766d1ed9044ed2fcadccf7ff
Fuad Mustamirrul Ishlah 83 <amir24405@gmail.com>
2019-09-05 09:48:42 +0700

    changing local to remote

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 
 const mysql = require('mysql')
 const conn = mysql.createConnection({
-  host: process.env.DB_LOCAL_HOST,
-  user: process.env.DB_LOCAL_USER,
-  password: process.env.DB_LOCAL_PASSWORD,
-  database: process.env.DB_LOCAL_NAME
+  host: process.env.DB_REMOTE_HOST,
+  user: process.env.DB_REMOTE_USER,
+  password: process.env.DB_REMOTE_PASSWORD,
+  database: process.env.DB_REMOTE_NAME
 })
 module.exports =  conn
